d5c345433c2bbc8f2ba1760a0edfa8f5b4f0e96e
Christian Di Troia <christian.ditroia@outlook.com>
2025-07-26 20:49:10 -0400

    Created threat analyzer and threat analysis classes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from ThreatAnalyzer import ThreatAnalyzer
 
+threat_analyzer = ThreatAnalyzer()
 app = FastAPI()
 
 
@@ -9,23 +11,26 @@ class ThreatDetectionRequest(BaseModel):
 
 
 class ThreatDetectionResponse(BaseModel):
-    threat_detected: bool
-    emotion: str
-    suspicious_text: list[str]
-    processed_text: str
+    negative_sentiments: int
+    emotions: list[str]
+    danger_words: list[str]
 
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Threat Detector API"}
+    return {
+        "message": "Welcome to the Threat Detector API. Post to /detect-threat with {text: str } to analyze text."
+    }
 
 
 @app.post("/detect-threat")
-async def detect_threat(data: ThreatDetectionRequest):
+async def detect_threat(data: ThreatDetectionRequest) -> ThreatDetectionResponse:
+    analysis = threat_analyzer.perform_analysis(data.text)
+
     response = ThreatDetectionResponse(
-        threat_detected=False,
-        emotion="neutral",
-        suspicious_text=["Testing123", "Attacking at dawn"],
-        processed_text=data.text,
+        negative_sentiments=analysis.get_negative_sentiments(),
+        emotions=analysis.get_emotions(),
+        danger_words=analysis.get_danger_words(),
     )
+
     return response
